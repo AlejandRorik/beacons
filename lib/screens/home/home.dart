172215ff15a03096f61811d18583511773c73665
AlejandRorik <xx.alejandrorick.xx@gmail.com>
2020-09-12 13:03:44 +0200

    codigo de creacion de documentos

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,15 +1,19 @@
+
 import 'dart:io';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'dart:async';
+
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:beacons/services/AuthService.dart';
 import 'package:beacons/shared/constants.dart';
 import 'package:beacons/shared/screenSize.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_beacon/flutter_beacon.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import '../../app_localizations.dart';
+import 'package:intl/intl.dart';
 import 'package:beacons/models/beacon.dart';
 
 class Home extends StatefulWidget {
@@ -21,18 +25,21 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> with WidgetsBindingObserver{
   //variables del Home
   final AuthService _auth = AuthService();
+  final db = Firestore.instance;
   final StreamController<BluetoothState> streamController = StreamController();
   StreamSubscription<BluetoothState> _streamBluetooth;
   StreamSubscription<RangingResult> _streamRanging;
 
   final _regionBeacons = <Region, List<Beacon>>{};
   final _beacons = <Beacon>[];
+  final _beaconsTrue = <Beacon,DateTime>{};
   bool authorizationStatusOk = false;
   bool locationServiceEnabled = false;
   bool bluetoothEnabled = false;
 
   @override
   void initState() {
+    AuthService().getCurrentUser();
     WidgetsBinding.instance.addObserver(this);
     super.initState();
     listeningState();
@@ -103,17 +110,34 @@ class _HomeState extends State<Home> with WidgetsBindingObserver{
       }
     }
 
-    _streamRanging = flutterBeacon.ranging(regions).listen((RangingResult result) {
+    _streamRanging = flutterBeacon.ranging(regions).listen((RangingResult result) async{
           if (result != null && mounted) {
             setState(() {
               _regionBeacons[result.region] = result.beacons;
-              _beacons.clear();
+              _beacons!=null ? _beacons.clear() : null;
               _regionBeacons.values.forEach((list) {
                 _beacons.addAll(list);
               });
               _beacons.sort(_compareParameters);
-              /*aqui tengo que comprobar que los beacons que hay en la lista coincidan con los de la base de datos y si es asi hacer un documento que deje prueba de ello*/
-              print(_beacons);
+            });
+
+            /*aqui tengo que comprobar que los beacons que hay en la lista coincidan con los de la base de datos y si es asi hacer un documento que deje prueba de ello*/
+            _beacons.forEach((element) {
+              if(!_beaconsTrue.keys.contains(element)){
+                _beaconsTrue[element] = DateTime.now();
+              }
+            });
+            _beaconsTrue.forEach((key, value) async {
+              var getBeaconOut = true;
+              _beacons.forEach((element) {
+                if(key==element){
+                  getBeaconOut = false;
+                }
+              });
+              if(getBeaconOut == true){
+                await makeBeaconDocument(key,value);
+                _beaconsTrue.remove(key);
+              }
             });
           }
         });
@@ -151,6 +175,17 @@ class _HomeState extends State<Home> with WidgetsBindingObserver{
     });
   }
 
+  makeBeaconDocument(Beacon beacon, DateTime entrada) async{
+    if(AuthService.currentId!=null){
+      await db.collection('usuarios/' + AuthService.currentId + '/userPassingBy').add({
+        'beaconUUID': beacon.proximityUUID,
+        'beaconMAC': beacon.macAddress,
+        'horaEntradaEnRango': entrada,
+        'horaSalidaDeRango': DateTime.now(),
+      });
+    }
+  }
+
   //didChangeAppLifecycleState() inicia todas las funciones anteriores dependiendo de si tenemos permisos necesarios o no
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) async {
@@ -186,6 +221,8 @@ class _HomeState extends State<Home> with WidgetsBindingObserver{
   //build de home
   @override
   Widget build(BuildContext context) {
+    //DateFormat.Hms().format(DateTime.now())
+
     ScreenSize().init(context);
     return Container(
       decoration: BoxDecoration(
@@ -265,39 +302,65 @@ class _HomeState extends State<Home> with WidgetsBindingObserver{
             FlatButton.icon(
               icon: Icon(Icons.person, size: ScreenSize.blockSizeHorizontal * 5, color: Colors.black87),
               label: Text(AppLocalizations.of(context).translate('logOutAppBar'),style: TextStyle(fontSize: ScreenSize.blockSizeHorizontal * 3.5, color: Colors.black87)),
-              onPressed: () async{
+              onPressed: () async {
                 await _auth.signOut();
               },
             ),
           ],
         ),
-        body: _beacons == null || _beacons.isEmpty
+        body: authorizationStatusOk==false || locationServiceEnabled==false || bluetoothEnabled==false
             ? Center(
-            child: SpinKitCircle(color: appBackgroundColor1)
+              child: Container(
+                  height: ScreenSize.blockSizeVertical * 15,
+                  width: ScreenSize.blockSizeHorizontal * 60,
+                  child: Card(
+                      elevation: 3,
+                      child: Center(
+                          child: Text("Compruebe que la aplicación tiene todos los permisos en orden (iconos de arriba)",textAlign: TextAlign.center, style: TextStyle(fontSize: ScreenSize.blockSizeHorizontal * 4),)
+                      )
+                  ),
+        ),
             )
+            : _beaconsTrue == null || _beaconsTrue.isEmpty
+            ? Center(child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SpinKitWave(color: Colors.white70,size: ScreenSize.blockSizeVertical*10,),
+                SizedBox(height: ScreenSize.blockSizeVertical*3,),
+                Text("Escaneando...",style: TextStyle(color: Colors.white70,fontSize: ScreenSize.blockSizeVertical*4),)
+              ],
+            ))
             : ListView(
           children: ListTile.divideTiles(
               context: context,
-              tiles: _beacons.map((beacon) {
+              tiles: _beaconsTrue.keys.map((beacon) {
                 return Card(
                   elevation: 4,
                   child: ListTile(
-                    title: Text('UUID: ${beacon.proximityUUID}',textAlign: TextAlign.left,style: TextStyle(fontSize: ScreenSize.blockSizeHorizontal*4),),
+                    title: Text('UUID: ${beacon.proximityUUID}',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: ScreenSize
+                          .blockSizeHorizontal * 4),),
                     subtitle: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        new Text('MAC: ${beacon.macAddress}', textAlign: TextAlign.end,),
+                        new Text('MAC: ${beacon.macAddress}',
+                          textAlign: TextAlign.end,),
                         new Row(
                           mainAxisSize: MainAxisSize.max,
                           children: <Widget>[
                             Flexible(
-                                child: Text('Major: ${beacon.major}\nMinor: ${beacon.minor}',
+                                child: Text(
+                                    'Major: ${beacon.major}\nMinor: ${beacon
+                                        .minor}',
                                     style: TextStyle(fontSize: 13.0)),
                                 flex: 1,
                                 fit: FlexFit.tight),
                             Flexible(
                                 child: Text(
-                                    'Accuracy: ${beacon.accuracy}m\nRSSI: ${beacon.rssi}',
+                                    'Accuracy: ${beacon
+                                        .accuracy}m\nRSSI: ${beacon.rssi}',
                                     style: TextStyle(fontSize: 13.0)),
                                 flex: 2,
                                 fit: FlexFit.tight)
